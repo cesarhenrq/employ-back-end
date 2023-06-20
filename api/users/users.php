38c99bbf531255bdfaba60bd5dfcedfc3583bdfe
cesarhenrq <cesaarhvb@gmail.com>
2023-06-20 02:53:32 -0300

    feat: implement users route

--- api/users/users.php
+++ api/users/users.php
@@ -1,7 +1,10 @@
 <?php
  if ($api == 'users') {
-    if ($method == 'GET') {
-        var_dump("it works!");
-        $rs = $db->prepare("SELECT * FROM users ORDER BY name");
+    if ($method == 'GET' && $action == 'get' && $param) {
+       include_once "get.php";
+    } elseif ($method == 'POST' && $action == 'create') {
+       include_once "create.php";
+    } else {
+        echo "Path doesn't exist!";
     }
 }
