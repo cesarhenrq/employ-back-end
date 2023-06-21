a4f243846792185cd96ab2965a187cd4ffa6b773
cesarhenrq <cesaarhvb@gmail.com>
2023-06-21 08:30:31 -0300

    feat: add pagination

--- api/tasks/get.php
+++ api/tasks/get.php
@@ -12,9 +12,13 @@ try {
     $decodedToken = JWT::decode($token, $secretJWT);
     $user_id = $decodedToken->id;
 
-    $db = DB::connect();
-    $stmt = $db->prepare("SELECT * FROM tasks WHERE user_id = :user_id");
+    $currentPage = $_GET['page'] ?? 1;
+    $pageSize = $_GET['size'] ?? 10;
+
+    $stmt = $db->prepare("SELECT * FROM tasks WHERE user_id = :user_id LIMIT :limit OFFSET :offset");
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+    $stmt->bindValue(':limit', $pageSize, PDO::PARAM_INT);
+    $stmt->bindValue(':offset', ($currentPage - 1) * $pageSize, PDO::PARAM_INT);
     $stmt->execute();
     $tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
