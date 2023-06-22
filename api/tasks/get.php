a6227a09c1b5f1f8f92e0d038a2d686483256b66
César Henrique <cesaarhvb@gmail.com>
2023-06-21 22:31:06 -0300

    Update get.php

--- api/tasks/get.php
+++ api/tasks/get.php
@@ -12,15 +12,15 @@ try {
     $decodedToken = JWT::decode($token, $secretJWT);
     $user_id = $decodedToken->id;
 
-    echo json_encode(["message"] => "Passou aqui"]);
+    echo json_encode(["message" => "Passou aqui"]);
     $stmt = $db->prepare("SELECT * FROM tasks WHERE user_id = :user_id");
-    echo json_encode(["message"] => "Passou aqui"]);
+    echo json_encode(["message" => "Passou aqui"]);
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
-    echo json_encode(["message"] => "Passou aqui"]);
+    echo json_encode(["message" => "Passou aqui"]);
     $stmt->execute();
-    echo json_encode(["message"] => "Passou aqui"]);
+    echo json_encode(["message" => "Passou aqui"]);
     $tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode(["message"] => "Passou aqui"]);
+    echo json_encode(["message" => "Passou aqui"]);
 
     if ($tasks) {
         echo json_encode(["data" => $tasks]);
