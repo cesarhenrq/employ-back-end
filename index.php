61e14cbb6af78a342da7211489dfd48848e381c5
César Henrique <cesaarhvb@gmail.com>
2023-06-21 21:20:12 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Max-Age: 86400');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
 header('Content-Type: application/json');
 
 date_default_timezone_set('America/Sao_Paulo');
